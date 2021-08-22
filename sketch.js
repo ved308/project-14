76417a6f5b3f1fd3563aa3eff3c2261d5632dd78
ved308 <85893002+ved308@users.noreply.github.com>
2021-08-22 21:34:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function setup() {
   //creating background
   scene = createSprite(0,0,400,400);
   scene.addImage(backgroundImage);
-  scene.scale = 1.5
+  scene.scale = 2.5
   
   // creating bow to shoot arrow
   bow = createSprite(380,220,20,50);
